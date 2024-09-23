51c43998e11ec0195c4bf1d268d7ca00d4ce7b85
Andrii Dzhus <158450996+Andrii-Dzhus@users.noreply.github.com>
2024-09-23 08:37:56 +0300

    8:30

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "modern-normalize";
-import App from "./components/App/App";
-import "./index.css";
-import { store } from "./redux/store";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import App from "./components/App/App.jsx";
+import "./index.css";
+import { store } from "./redux/store.js";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </StrictMode>
 );
